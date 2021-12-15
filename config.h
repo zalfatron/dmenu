9e67fc5c5736ed620a058429ab50c1645ac8c3a7
zalfatron <zalfatron@gmail.com>
2021-12-15 05:50:36 -0600

    Make dmenu display options in in a vertical menu

--- config.h
+++ config.h
@@ -8,13 +8,13 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
+	/*              fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 4;
 
 /*
  * Characters not considered part of a word while deleting words
